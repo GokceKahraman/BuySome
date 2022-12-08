6665d2ac6b4aa16f91cb9f769a5957278d73a6db
GokceKahraman <gokcekahraman0@gmail.com>
2022-12-08 23:47:19 +0300

    Initial Commit

---
+++ BuySome/BuySomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuySomeApp.swift
+//  BuySome
+//
+//  Created by Gökçe Kahraman on 8.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct BuySomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
